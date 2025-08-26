a03c37995c428045909293fd130cafb911f93728
XXX XXXXXX <michal.kurowski@edu.uekat.pl>
2025-08-26 22:03:53 +0200

    Methods tests without spies

--- src/app/add-recipe/add-recipe.component.spec.ts
+++ src/app/add-recipe/add-recipe.component.spec.ts
@@ -13,6 +13,7 @@ import { of } from 'rxjs';
 import { Router } from '@angular/router';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppModule } from '../app.module';
+import { tuiObjectFromEntries } from '@taiga-ui/cdk';
 
 describe('AddRecipeComponent', () => {
   let component: AddRecipeComponent;
@@ -102,112 +103,113 @@ describe('AddRecipeComponent', () => {
     });
   });
 
-  // describe('Testing methods', () => {
-  //   it('addIngredientValidator - should return true if ingredients form is invalid', () => {
-  //     component.ingredientForm.setValue({
-  //       name: null,
-  //       amount: null,
-  //       type: KitchenMeasures.DECAGRAM,
-  //     });
-  //     expect(component.addIngredientValidator()).toBeTrue();
-  //   });
+  describe('Testing methods', () => {
+    it('addIngredientValidator - should return true if ingredients form is invalid', () => {
+      component.ingredientForm.setValue({
+        name: null,
+        amount: null,
+        type: KitchenMeasures.DECAGRAM,
+      });
+      expect(component.addIngredientValidator()).toBeTrue();
+    });
 
-  //   it('addAndResetIngredient - should add ingredient and reset fields', () => {
-  //     component.ingredientForm.setValue({
-  //       name: 'Bread',
-  //       amount: 600,
-  //       type: KitchenMeasures.GRAM,
-  //     });
-  //     component.addAndResetIngredient();
-  //     fixture.detectChanges();
+    it('addAndResetIngredient - should add ingredient and reset fields', () => {
+      component.ingredientForm.setValue({
+        name: 'Bread',
+        amount: '600',
+        type: KitchenMeasures.GRAM,
+      });
+      component.addAndResetIngredient();
+      fixture.detectChanges();
 
-  //     expect(component.ingredientsArray.length).toBe(1);
-  //     expect(component.ingredientsArray[0].name).toBe('Bread');
-  //     expect(component.ingredientForm.value.name).toBeNull();
-  //   });
+      expect(component.ingredientsArray.length).toBe(1);
+      expect(component.ingredientsArray[0].name).toBe('Bread');
+      expect(component.ingredientsArray[0].amount).toBe('600');
+      expect(component.ingredientForm.value.name).toBeNull();
+    });
 
-  //   it('disableTagButtonIfEmpty - should disable tag button when tag is empty or error exists', () => {
-  //     component.tag = '   ';
-  //     expect(component.disableTagButtonIfEmpty()).toBeTrue();
+    it('disableTagButtonIfEmpty - should disable tag button when tag is empty or error exists', () => {
+      component.tag = '   ';
+      expect(component.disableTagButtonIfEmpty()).toBeTrue();
 
-  //     component.tag = 'Test';
-  //     component.displayError = 'Error';
-  //     expect(component.disableTagButtonIfEmpty()).toBeTrue();
-  //   });
+      component.tag = 'Test';
+      component.displayError = 'Error';
+      expect(component.disableTagButtonIfEmpty()).toBeTrue();
+    });
 
-  //   it('addTag - method push tag to tag array and reset tag control', () => {
-  //     component.tag = 'New tag';
-  //     component.recipeForm.get('tags')?.setValue([]);
+    it('addTag - method push tag to tag array and reset tag control', () => {
+      component.tag = 'New tag';
+      component.recipeForm.get('tags')?.setValue([]);
 
-  //     component.addTag();
+      component.addTag();
 
-  //     const tagsArray = component.recipeForm.get('tags')?.value;
-  //     expect(tagsArray).toContain('New tag');
-  //     expect(component.tag).toBe('');
-  //   });
+      const tagsArray = component.recipeForm.get('tags')?.value;
+      expect(tagsArray).toContain('New tag');
+      expect(component.tag).toBe('');
+    });
 
-  //   it('createRecipe - should not call method if form is invalid', () => {
-  //     component.recipeForm.get('name')?.setValue('');
-  //     component.createRecipe();
-  //     expect(recipesServiceSpy.createRecipe).not.toHaveBeenCalled();
+    //   it('createRecipe - should not call method if form is invalid', () => {
+    //     component.recipeForm.get('name')?.setValue('');
+    //     component.createRecipe();
+    //     expect(recipesServiceSpy.createRecipe).not.toHaveBeenCalled();
 
-  //     component.recipeForm.get('name')?.setValue('Dumplings');
-  //     component.recipeForm.get('description')?.setValue('Are very delicious');
-  //     expect(recipesServiceSpy.createRecipe).not.toHaveBeenCalled();
+    //     component.recipeForm.get('name')?.setValue('Dumplings');
+    //     component.recipeForm.get('description')?.setValue('Are very delicious');
+    //     expect(recipesServiceSpy.createRecipe).not.toHaveBeenCalled();
 
-  //     component.ingredientsArray.length = 0;
-  //     expect(recipesServiceSpy.createRecipe).not.toHaveBeenCalled();
-  //   });
+    //     component.ingredientsArray.length = 0;
+    //     expect(recipesServiceSpy.createRecipe).not.toHaveBeenCalled();
+    //   });
 
-  //   it('createRecipe - successed created recipe and navigate to next page', () => {
-  //     component.recipeForm.get('name')?.setValue('Cake');
-  //     component.recipeForm
-  //       .get('description')
-  //       ?.setValue('Who want one more piece?');
-  //     component.ingredientsArray = [
-  //       { name: 'Flour', amount: '600', type: KitchenMeasures.GRAM },
-  //     ];
-  //     //Check what's wrong with of({})
-  //     //recipesServiceSpy.createRecipe.and.returnValue(of({}));
+    //   it('createRecipe - successed created recipe and navigate to next page', () => {
+    //     component.recipeForm.get('name')?.setValue('Cake');
+    //     component.recipeForm
+    //       .get('description')
+    //       ?.setValue('Who want one more piece?');
+    //     component.ingredientsArray = [
+    //       { name: 'Flour', amount: '600', type: KitchenMeasures.GRAM },
+    //     ];
+    //     //Check what's wrong with of({})
+    //     //recipesServiceSpy.createRecipe.and.returnValue(of({}));
 
-  //     component.createRecipe();
+    //     component.createRecipe();
 
-  //     expect(recipesServiceSpy.createRecipe).toHaveBeenCalled();
-  //     expect(routerSpy.navigate).toHaveBeenCalledWith(['/']);
-  //   });
+    //     expect(recipesServiceSpy.createRecipe).toHaveBeenCalled();
+    //     expect(routerSpy.navigate).toHaveBeenCalledWith(['/']);
+    //   });
 
-  //   it('recipeNameValidation - block typing recipe name if it is longer than 40 characters', () => {
-  //     const longName = 'a'.repeat(40);
-  //     component.recipeForm.get('name')?.setValue(longName);
+    //   it('recipeNameValidation - block typing recipe name if it is longer than 40 characters', () => {
+    //     const longName = 'a'.repeat(40);
+    //     component.recipeForm.get('name')?.setValue(longName);
 
-  //     const event = new KeyboardEvent('keydown', { key: 'a' });
-  //     spyOn(event, 'preventDefault');
+    //     const event = new KeyboardEvent('keydown', { key: 'a' });
+    //     spyOn(event, 'preventDefault');
 
-  //     component.recipeNameValidation(event);
+    //     component.recipeNameValidation(event);
 
-  //     expect(event.preventDefault).toHaveBeenCalled();
-  //   });
+    //     expect(event.preventDefault).toHaveBeenCalled();
+    //   });
 
-  //   it('preventTagRepetiton - show error if tag is already added', () => {
-  //     const tags = ['Vegan', 'Quick'];
-  //     component.recipeForm.get('tags')?.setValue(tags);
+    //   it('preventTagRepetiton - show error if tag is already added', () => {
+    //     const tags = ['Vegan', 'Quick'];
+    //     component.recipeForm.get('tags')?.setValue(tags);
 
-  //     component.preventTagRepetiton('Quick');
+    //     component.preventTagRepetiton('Quick');
 
-  //     expect(component.displayError).toBe('This tag already exists');
-  //   });
+    //     expect(component.displayError).toBe('This tag already exists');
+    //   });
 
-  //   it('preventTagRepetiton - clear error if tag is new', () => {
-  //     component.recipeForm.get('tags')?.setValue(['Easy']);
-  //     component.preventTagRepetiton('New');
+    //   it('preventTagRepetiton - clear error if tag is new', () => {
+    //     component.recipeForm.get('tags')?.setValue(['Easy']);
+    //     component.preventTagRepetiton('New');
 
-  //     expect(component.displayError).toBeNull();
-  //   });
+    //     expect(component.displayError).toBeNull();
+    //   });
 
-  //   it('ngOnDestroy - unsubscribe on destroy', () => {
-  //     const destroySpy = spyOn(component['destroySubscribe$'], 'unsubscribe');
-  //     component.ngOnDestroy();
-  //     expect(destroySpy).toHaveBeenCalled();
-  //   });
-  // });
+    //   it('ngOnDestroy - unsubscribe on destroy', () => {
+    //     const destroySpy = spyOn(component['destroySubscribe$'], 'unsubscribe');
+    //     component.ngOnDestroy();
+    //     expect(destroySpy).toHaveBeenCalled();
+    //   });
+  });
 });
